3e902cf6429d7229202fb18decdddf9a42f21604
Gabriel Henrique Linke <gh.linke@hotmail.com>
2021-11-29 18:49:31 -0300

    Show time taken

--- tsp.cpp
+++ tsp.cpp
@@ -7,17 +7,11 @@ Thiago de Mendonça Mildemberger - 1716980
 #include <vector>
 #include <string>
 #include <cmath>
+#include <iomanip>
 
 #include <fstream>
 
-using std::cerr;
-using std::cout;
-using std::endl;
-
-using std::ifstream;
-using std::string;
-using std::vector;
-
+using namespace std;
 struct Vertex
 {
     int x;
@@ -32,6 +26,7 @@ vector<Vertex> readVertices(string inputFile)
         cerr << "Arquivo não pode ser aberto" << endl;
         fflush(stdin);
         getchar();
+        exit(1);
     }
     vector<Vertex> vertices;
     int size = 0;
@@ -54,13 +49,22 @@ double getEuclideanDistance(Vertex &v1, Vertex &v2)
 int main(int argc, char *argv[])
 {
     if (argc != 2)
+    {
+        cout << "Utilize ./tsp input.txt" << endl;
         return 1;
+    }
+
+    time_t start = clock();
 
     vector<Vertex> vertices = readVertices(argv[1]);
     for (int i = 0; i < vertices.size(); i++)
     {
         cout << vertices[i].x << " " << vertices[i].y << endl;
     }
+
+    time_t end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    cout << std::setprecision(6) << fixed << time_taken << /* custo do ciclo */ endl;
 
     return 0;
 }
