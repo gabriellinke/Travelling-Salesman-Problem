687487b9918fbab243d4836b5d5ce05b2cee39d3
Gabriel Henrique Linke <gh.linke@hotmail.com>
2021-11-29 21:20:55 -0300

    Calculate cost

--- tsp.cpp
+++ tsp.cpp
@@ -46,6 +46,26 @@ double getEuclideanDistance(Vertex &v1, Vertex &v2)
     return sqrt((v1.x - v2.x) * (v1.x - v2.x) + (v1.y - v2.y) * (v1.y - v2.y));
 }
 
+double calculateCost(vector<Vertex> &vertices)
+{
+    double cost = 0;
+    if (vertices.size() == 0)
+    {
+        cerr << "Ciclo não contém nenhum vértice" << endl;
+        fflush(stdin);
+        getchar();
+        exit(1);
+    }
+    if (vertices.size() == 1)
+        return 0;
+
+    for (int i = 1; i < vertices.size(); i++)
+    {
+        cost += getEuclideanDistance(vertices[i - 1], vertices[i]);
+    }
+    return cost;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -56,15 +76,20 @@ int main(int argc, char *argv[])
 
     time_t start = clock();
 
+    //--------------------------------------------------------------------------------
+    // Faz todos os processos aqui
     vector<Vertex> vertices = readVertices(argv[1]);
     for (int i = 0; i < vertices.size(); i++)
     {
         cout << vertices[i].x << " " << vertices[i].y << endl;
     }
+    vertices.push_back(vertices[0]);
+
+    //--------------------------------------------------------------------------------
 
     time_t end = clock();
     double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
-    cout << std::setprecision(6) << fixed << time_taken << /* custo do ciclo */ endl;
+    cout << std::setprecision(6) << fixed << time_taken << " " << calculateCost(vertices) << endl;
 
     return 0;
 }
